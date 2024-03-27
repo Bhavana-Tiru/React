0cabfb02a40bc54740334e35f5dfddf705d63b9a
Bhavana <tiruveedulabhavana96@gmail.com>
2024-03-28 01:52:29 +0530

    importing files

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import MovieDetails from "./components/MovieDetails/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:imdbId" element={<MovieDetails />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
