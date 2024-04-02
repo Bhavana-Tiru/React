e3562377dad428ae0cc57181f0e990af71ad28b3
Bhavana <tiruveedulabhavana96@gmail.com>
2024-04-03 04:32:44 +0530

    added movies

--- redux/src/features/movies/movieSlice.js
+++ redux/src/features/movies/movieSlice.js
@@ -1,4 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const response = await axios.get(`https://dummyapi.online/api/movies`);
+    // .catch((err) => {
+    //   console.log("Err :", err);
+    // });
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -9,14 +21,28 @@ const movieSlice = createSlice({
   initialState,
   reducers: {
     //here initialState is state and
-    addMovies: (state, { payload }) => {
-      // when we get movies form the payload
-      state.movies = payload;
-    },
+    // addMovies: (state, { payload }) => {
+    //   // when we get movies form the payload
+    //   state.movies = payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAsyncMovies.pending, () => {
+        console.log("Pending");
+      })
+      .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
+        console.log("Fetched Sucessfully!");
+        return { ...state, movies: payload };
+      })
+      .addCase(fetchAsyncMovies.rejected, () => {
+        console.log("Rejected");
+      });
   },
 });
 
 export const { addMovies } = movieSlice.actions;
+// export const { addshow } = showsSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 //if we want a value from the store
 //1st movie:- name of the slice here it is movies
